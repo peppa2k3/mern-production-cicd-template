476716b1ed3012a5cf28d4d82ba5415e6c1cc784
peppa2k3 <liup20204@gmail.com>
2026-08-20 09:28:09 +0700

    fix(CICD):devploy with SSH VPS

--- server/src/app.js
+++ server/src/app.js
@@ -25,6 +25,7 @@ app.use(
     credentials: true,
   })
 );
+console.log("env.clientUrl is :",env.clientUrl)
 app.use(compression());
 app.use(express.json({ limit: '2mb' }));
 app.use(express.urlencoded({ extended: true, limit: '2mb' }));
